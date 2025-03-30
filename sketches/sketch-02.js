7246ba54a94397d5466a83c336c0f55e3e032568
Lorena Bastos <lorenaax.bastos@hotmail.com>
2025-03-30 15:05:29 -0300

    feat arcs

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,7 +1,9 @@
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
-const degToRad = (deg) => (deg * Math.PI) / 180;
-const minMaxRandom = (min, max) => min + Math.random() * (max - min);
+// const degToRad = (deg) => (deg * Math.PI) / 180;
+// const randomRange = (min, max) => min + Math.random() * (max - min);
 
 const settings = {
     dimensions: [1080, 1080],
@@ -9,33 +11,55 @@ const settings = {
 
 const sketch = () => {
     return ({ context, width, height }) => {
-        context.fillStyle = "000";
-        context.fillRect(0, 0, width, height);
         context.fillStyle = "#fff";
+        context.fillRect(0, 0, width, height);
+        context.fillStyle = "#000";
+        context.strokeStyle = "#000";
 
         const [cx, cy] = [width / 2, height / 2];
         const ratioSize = 1 / 10;
         const [w, h] = [(width * ratioSize) / 10, height * ratioSize];
-        const n = 50;
+        const n = 40;
         const radius = width / 3;
         let x, y;
 
-        context.translate(cx, cy);
+        // context.translate(cx, cy);
 
         for (let i = 0; i < n; i++) {
-            const angle = degToRad(360 / n) * i;
-            x = Math.sin(angle) * radius;
-            y = Math.cos(angle) * radius;
+            const slice = math.degToRad(360 / n);
+            const angle = slice * i;
+            x = cx + Math.sin(angle) * radius;
+            y = cy + Math.cos(angle) * radius;
 
+            // rects
             context.save();
-            // context.scale(1, 2);
+
             context.translate(x, y);
+            context.rotate(-angle);
+            context.scale(random.range(0.5, 1.5), random.range(0.5, 1));
+
+            context.beginPath();
+            context.rect(-w / 2, random.range(0, -h / 2), w, h);
+            context.fill();
+
+            context.restore();
+
+            // arcs
+            context.save();
+
+            context.translate(cx, cy);
             context.rotate(-angle);
 
             context.beginPath();
-            context.scale(minMaxRandom(0.2, 2), minMaxRandom(0.2, 2));
-            context.rect(-w / 2, -h / 2, w, h);
-            context.fill();
+            context.lineWidth = random.range(1, 25);
+            context.arc(
+                0,
+                0,
+                radius * random.range(0.5, 1.4),
+                -slice * random.range(0, 15),
+                slice * random.range(0, 5)
+            );
+            context.stroke();
 
             context.restore();
         }
